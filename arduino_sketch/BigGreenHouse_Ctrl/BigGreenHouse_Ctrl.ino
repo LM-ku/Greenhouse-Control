61af8c4c3f9de20110751f4c097dbf33b161a962
LM-ku <41206570+LM-ku@users.noreply.github.com>
2019-04-26 00:19:50 +0500

    Update BigGreenHouse_Ctrl.ino

--- arduino_sketch/BigGreenHouse_Ctrl/BigGreenHouse_Ctrl.ino
+++ arduino_sketch/BigGreenHouse_Ctrl/BigGreenHouse_Ctrl.ino
@@ -5,79 +5,88 @@
 BME280 I2C (Температура воздуха, Давление, Влажность воздуха)
      Vin (Voltage In)     ->  3.3V
      Gnd (Ground)         ->  Gnd
-     SDA (Serial Data)    ->  JPIO4 (D2) on D1 Mini
-     SCK (Serial Clock)   ->  JPIO5 (D1) on D1 Mini
+     SDA (Serial Data)    ->  JPIO4 
+     SCK (Serial Clock)   ->  JPIO5 
 	 
 CAPACITIVE SOIL MOISTURE SENSOR (Влажность почвы)	 
-	 Vin (Voltage In)     ->  3.3V
+     Vin (Voltage In)     ->  3.3V
      Gnd (Ground)         ->  Gnd
-     AI  (Analog Input)   ->  ADC0 (A0) on D1 Mini 
+     AI  (Analog Input)   ->  ADC0 
 
 PULS FLOW SENSOR (Расход воды на полив)
-	 Vin (Voltage In)     ->  3.3V
+     Vin (Voltage In)     ->  3.3V
      Gnd (Ground)         ->  Gnd
-     DI  (Digital Input)  ->  JPIO13 (D7) on D1 Mini	 
+     DI  (Digital Input)  ->  JPIO16  1 puls / ... l water
 
 OUT VALVE RELAY (Орошение)
-     DO  (Digital Output) ->  JPIO16 (D0) on D1 Mini	 
+     DO  (Digital Output) ->  JPIO13 	 
 
 FAN RELAY (Вентиляция)
-     DO  (Digital Output) ->  JPIO14 (D5) on D1 Mini	
-
-IN VALVE RELAY (Наполнение бака с водой - Резерв)
-     DO  (Digital Output) ->  JPIO12 (D6) on D1 Mini
-	 
+     DO  (Digital Output) ->  JPIO15	
+     
+IRRIGATION TIMER
+     DI  (Digital Input)  ->  JPIO12  puls for START irrigation 
+     DI  (Digital Input)  ->  JPIO14  puls for STOP irrigation
 	 
 */
 
-// **** ИСПОЛЬЗУЕМЫЕ БИБЛИОТЕКИ 
-#include <ESP8266WiFi.h>					                // библиотека ESP8266WiFi
-#include <PubSubClient.h>					                // библиотека PubSubClient (MQTT)
-#include <ESP8266WebServer.h>				              // библиотека ESP8266WebServer
-#include <BME280I2C.h>              		          // библиотека ВМЕ280I2C
-// #include <OLED_I2C.h>             		          // библиотека OLED_I2C
-
-// **** НАСТРОЙКИ ПОДКЛЮЧЕНИЯ 
-const char* ssid     	    = "******";             // Название точки доступа     
-const char* password 	    = "******";             // Пароль                     
-const char* MQTT_server   = "192.168.123.222";
-String topic              = "BigGreenHouse";         
-#define MQTT_user 		"username"
-#define MQTT_password "password"
+// **** WiFi 
+#include <ESP8266WiFi.h>					      	// библиотека ESP8266WiFi
+const char* ssid  	= "******";             			// Название точки доступа     
+const char* password	= "******";             			// Пароль     
 
 
+// **** MQTT
+#include <PubSubClient.h>					      	// библиотека PubSubClient (MQTT)
+const char *mqtt_server = "192.168.123.222"; 
+const int mqtt_port 	= 1886; 
+const char *mqtt_user 	= "Login"; 
+const char *mqtt_pass 	= "Pass"; 
+String topic            = "BigGreenHouse"; 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
 
+#define MQTT_user 	"username"
+#define MQTT_password 	"password"
+
+// **** WEB cервер 
+#include <ESP8266WebServer.h>				              	// библиотека ESP8266WebServer
 
 
-     
-// **** НАСТРОЙКИ BME280
+// **** BME280
+#include <BME280I2C.h>              		          		// библиотека ВМЕ280I2C
 // bme(<Temperature Oversampling Rate>, <Humidity Oversampling Rate>, <Pressure Oversampling Rate>, <Mode>, <Standby Time>, <Filter>, <SPI Enable>, <BME280 Address>)
 // BME280I2C bme(0x1, 0x1, 0x1, 0x1, 0x5, 0x0, false, 0x77); 
-
 BME280I2C bme;								// По умолчанию
+float temp, hum, pres, moist;
 
-  
+
 // **** ОБЪЯВЛЕНИЕ ПЕРЕМЕННЫХ
+float set_temp, set_hum, set_moist;					// значения датчиков
 
-unsigned long PreTime;                                                              // отметка системного времени в предыдущем цикле
+int irrigation_valve 	= 13;                
+int ventilation_fan 	= 15;
+int irrigation_start	= 12;
+int irrigation_stop	= 14;
+int flow_meter 		= 16;  
+
+
+unsigned long PreTime;                                           	// отметка системного времени в предыдущем цикле
 unsigned long LastMsg = 0;
 unsigned long Last;
-long Cycle;                                                                         // мс, длительность цикла
+long Cycle;                                                             // мс, длительность цикла
 
-float temp, hum, pres, moist;														// значения датчиков
-//float temp(NAN), hum(NAN), pres(NAN);
+char msg[50];
+int value = 0;									
 
-float set_temp, set_hum, set_moist;
 
-int year, month, day_m, day_w, hour, minute, second, ms;							// переменные для учета времени
+
+
+
   
 
-int flow_meter 			  = 13;  
-int ventilation_fan 	= 14;
-int irrigation_valve 	= 16;
+
 
 
 
@@ -85,45 +94,53 @@ int irrigation_valve 	= 16;
 //          SETUP_WiFi - ПРОЦЕДУРА ПОДКЛЮЧЕНИЯ К WiFi
 // ------------------------------------------------------------
 void setup_wifi() {
+  int ct = 0;	
   delay(10);
-  Serial.print("Connecting to ");					                              // сообщение "Connecting to "
-  Serial.println(ssid);								                                  // <имя WiFi сети>
-  WiFi.mode(WIFI_STA);								                                  // старт подключения к WiFi
-  WiFi.begin(ssid, password);						                                // с ранее определенными <ssid> и <password>
-  while (WiFi.status() != WL_CONNECTED) {			                          // до тех пор, пока нет WiFi подключения
+  Serial.print("Connecting to ");					
+  Serial.println(ssid);							
+  WiFi.mode(WIFI_STA);	
+  WiFi.begin(ssid, password);		
+  while (WiFi.status() != WL_CONNECTED && ct < 5) {			// не более 5 попыток подключения к WiFi
     delay(500);
-    Serial.print(".");								                                  // сообщение "..... "
+    Serial.print(".");						       	// сообщение "..... "
+    randomSeed(micros());
+    ct += 1;	  
   }
-  Serial.println("");								                                    // после установления соединения с WiFi
-  Serial.println("WiFi connected");					                            // сообщение "WiFi connected" 
-  Serial.print("IP address: ");						                              // сообщение "IP address: "
-  Serial.println(WiFi.localIP());					                              // <WiFi.localIP>
+  Serial.println("");	
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("WiFi connected");					// сообщение "WiFi connected" 
+    Serial.print("IP address: ");					// сообщение "IP address: "
+    Serial.println(WiFi.localIP());					// <WiFi.localIP>
+  }
+  else {
+    Serial.println("WiFi NOT connected");				// сообщение "WiFi NOT connected" 
+  }
 }
-
 
 // ------------------------------------------------------------
 //           RECONNECT - ПРОЦЕДУРА ПОДКЛЮЧЕНИЯ К MQTT
 // ------------------------------------------------------------
 void reconnect() {
-  while (!client.connected()) {						                              // до тех пор, пока нет подключения к MQTT
-    Serial.print("Attempting MQTT connection...");	                    // сообщение "Attempting MQTT connection..."                   
+  int ct = 0;	
+  while (!client.connected() && ct < 10) {				// не более 10 попыток подключения к брокеру		                              // до тех пор, пока нет подключения к MQTT
+    Serial.print("Attempting MQTT connection...");	                // сообщение "Attempting MQTT connection..."                   
     char clientId[] = "BigGreenHouse";                                  // ID MQTT-клиента 
 //  if (client.connect(clientId,MQTT_user,MQTT_password)) {    
     if (client.connect(clientId)){                                      // если клиент подключился к брокеру,  
       Serial.println("connected");                                      // сообщение "connected"
-      String reseiver = topic + "/cmd/#";                               
-      client.subscribe(reseiver.c_str());                               // подписка на избранные топики
-      client.subscribe((topic + "/cmd/#").c_str());
+      client.subscribe((topic + "/cmd/#").c_str());			// подписка на избранные топики
     } 
     else {										                                          // иначе,
-      Serial.print("failed, rc=");					                            // сообщение "failed, rc="
-      Serial.print(client.state());					                            // <код состояния>
-      Serial.println(" try again in 5 seconds");	                      // сообщение " try again in 5 seconds"
-      delay(5000);									                                    // пауза перед повторным соединением 
+      Serial.print("failed, rc=");					// сообщение "failed, rc="
+      Serial.print(client.state());					// <код состояния>
+      Serial.println(" try again in 5 seconds");	                // сообщение " try again in 5 seconds"
+      delay(5000);							// пауза перед повторным соединением 
     }
+    ct += 1;
   }
 }
 
+// ------------------------------------------------------------
 // ------------------------------------------------------------
 //        CALLBACK_MQTT - ПРОЦЕДУРА ПОЛУЧЕНИЯ СООБЩЕНИЙ
 // ------------------------------------------------------------
@@ -135,7 +152,7 @@ void callback_mqtt(char* topic, byte *payload, unsigned int length) {
     Serial.write(payload, length);
     Serial.println();
 }
-
+// ------------------------------------------------------------
 
 
 // ------------------------------------------------------------
@@ -143,21 +160,16 @@ void callback_mqtt(char* topic, byte *payload, unsigned int length) {
 // ------------------------------------------------------------
 
 void setup() {                  
-  PreTime = millis();                                                               // сохранить системное время 
+  PreTime = millis();                                              	// сохранить системное время 
 
-  // **** ИНИЦИАЛИЗАЦИЯ ВЫВОДОВ
-  pinMode(LED_BUILTIN, OUTPUT);				            // встроенный светодиод
-  digitalWrite(LED_BUILTIN, HIGH);			          // зажечь
-  delay(100);								                      // на 100 ms
-  digitalWrite(LED_BUILTIN, LOW);			            // погасить  
-  
-
-  pinMode(flow_meter, INPUT);				              // подключение расходомера
-
-  pinMode(12, OUTPUT);						                // подключение реле клапана наполнения бака
-  pinMode(ventilation_fan, OUTPUT);			          // подключение реле вентиляции
-  pinMode(irrigation_valve, OUTPUT);  		        // подключение реле клапана полива
+// **** ИНИЦИАЛИЗАЦИЯ ВЫВОДОВ
+  pinMode(LED_BUILTIN, OUTPUT);				            	// встроенный светодиод
+  pinMode(ventilation_fan, OUTPUT);			          	// подключение реле вентиляции
+  pinMode(irrigation_valve, OUTPUT);  		        		// подключение реле клапана полива
  
+  pinMode(flow_meter, INPUT);				              	// подключение расходомера
+  pinMode(irrigation_start, INPUT);
+  pinMode(irrigation_stop, INPUT);
   
 // **** SERIAL ПОРТ
   Serial.begin(9600);						 
@@ -166,20 +178,21 @@ void setup() {
   setup_wifi();
   
 // **** MQTT 
-  client.setServer(MQTT_server, 1883);
+  client.setServer(mqtt_server, mqtt_port);
   reconnect();
   
 // **** BME280  
-  while (!bme.begin()) {            		        // до тех пор, пока BME280 не обнаружен   
-    Serial.println ("bme280 not found");        // сообщение "bme280 not found"               
-    digitalWrite(LED_BUILTIN, HIGH);            
-    delay(500);								                  // мигать
-    digitalWrite(LED_BUILTIN, LOW);             // встроенным светодиодом
-    delay(500);								                  // с частотой 1 Hz
+  while (!bme.begin() && int ct < 30) {            		        // до тех пор, пока BME280 не обнаружен   
+    Serial.println ("bme280 not found");        			// сообщение "bme280 not found"               
+    digitalWrite(LED_BUILTIN, LOW);            			
+    delay(500);								// мигать 30 s
+    digitalWrite(LED_BUILTIN, HIGH);             			// встроенным светодиодом
+    delay(500);								// с частотой 1 Hz
+    ct += 1:
   }
 
   delay(500);  
-  PreTime = millis();               		        // отметка времени в конце цикла программы 
+  PreTime = millis();               		        		// отметка времени в конце цикла программы 
 }
 
 // ------------------------------------------------------------
@@ -188,8 +201,8 @@ void setup() {
 
 void loop() {                   
 
-  if (!client.connected()) {				            // если нет подключения к MQTT брокеру, 
-    reconnect();							                  // переподключение и подписка на топики
+  if (!client.connected()) {				            	// если нет подключения к MQTT брокеру, 
+    reconnect();							// переподключение и подписка на топики
   } 
   client.loop();							                  // MQTT клиент ...
   long NowTime = millis();
@@ -261,56 +274,12 @@ void loop() {
     client.publish ("BigGreenHouse/info/Humidity", MsgHum);
     client.publish ("BigGreenHouse/info/Pressure", MsgPres);
     client.publish ("BigGreenHouse/info/Soil Moisture", MsgMoist);
-	  client.publish ("BigGreenHouse/info/Water Flow", MsgFlow);
+    client.publish ("BigGreenHouse/info/Water Flow", MsgFlow);
     client.publish ("BigGreenHouse/info/Ventilation", MsgVent.c_str(),true);
     client.publish ("BigGreenHouse/info/Irrigation", MsgIrrig.c_str(),true);
   }
 
-// **** СИСТЕМНОЕ ВРЕМЯ  
-  Cycle = NowTime - PreTime;                                                        // прошло времени с предыдущего цикла программы 
-  ms = ms + Cycle;
-  if (ms >= 1000) {
-	ms -= 1000;
-	second += 1;
-  }
-  if (second >= 60) {
-    second -= 60;
-    minute += 1;
-  }
-  if (minute >=60) {
-    minute -=60;
-	  hour += 1;
-  }
-  if (hour >= 24) {
-    hour -= 24;
-	  day_m += 1;
-    day_w += 1;
-  }
-  if (day_w >= 8) {
-    day_w = 1;
-  }
-  if ((day_m >= 28) && (month == 2) && (year % 4 != 0)) {
-    day_m -= 28;
-	  month = 3;
-  }
-  if ((day_m >= 29) && (month == 2) && (year % 4 == 0)) {
-    day_m -= 29;
-	  month = 3;
-  }
-  if ((day_m >= 30) && ((month == 4) || (month == 6) || (month == 9) || (month == 11))) {
-    day_m -= 30;
-	  month += 1;
-  }
-  if ((day_m >= 31) && ((month == 1) || (month == 3) || (month == 5) || (month == 7) || (month == 8) || (month == 10))) {
-    day_m -= 31;
-	  month += 1;
-  }
-  if ((day_m >= 31) && (month == 12))  {
-    day_m -= 31;
-	  month = 1;
-	  year += 1;
-  }
-  PreTime = NowTime; 
+
   
   
 }
